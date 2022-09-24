4c8a0e09ebe7fb73f6595350565e32ee945bba67
Galina Shuravina <shuravinags@mail.ru>
2022-09-24 20:54:23 +0700

    add build gradle

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,46 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void testRemainIfUnder1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+
+    }
+
+    @Test
+    public void testRemainIfAbove1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+
+    }
+    @Test
+    public void testRemainIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+
+    }
 }
